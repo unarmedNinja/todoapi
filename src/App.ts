54fc7bed00e24022dc20093da7cccda4dfb041b9
unarmedninja <matthew.swamy@gmail.com>
2018-10-31 10:38:56 -0400

    enable CORS

--- src/App.ts
+++ src/App.ts
@@ -27,6 +27,12 @@ class App {
     this.express.use(bodyParser.urlencoded({ extended: true }));
     this.express.use(bodyParser.json());    
     
+    //allow CORS
+    this.express.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
   }
 
 }
