185401757c9aee210db7261cabedcbd8d48cb459
unarmedninja <matthew.swamy@gmail.com>
2018-10-31 13:30:14 -0400

    update CORS methods

--- src/App.ts
+++ src/App.ts
@@ -31,6 +31,7 @@ class App {
     this.express.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS, HEAD, DELETE");
       next();
     });
   }
